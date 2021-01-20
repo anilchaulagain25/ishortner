ffa8c13f5e9ae1e48f728c49c81e231cad58f6ca
nepathya <anilchaulagain25@gmail.com>
2021-01-20 12:43:49 +0545

    callback return fixes

--- public/app.js
+++ public/app.js
@@ -1,33 +1,34 @@
 //utils
-var utils = {    
-    getUuidv4:  function () {
+var utils = {
+    getUuidv4: function() {
         return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-            var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+            var r = Math.random() * 16 | 0,
+                v = c == 'x' ? r : (r & 0x3 | 0x8);
             return v.toString(16);
         });
     },
-    overlay:{
-        on:function(text, disapperInSeconds){
+    overlay: {
+        on: function(text, disapperInSeconds) {
             document.getElementById("overlay").style.display = "block";
-            if(text)
-            $("#overlay span").text(text);
-            if(disapperInSeconds)
-            setTimeout(utils.overlay.off, 1000* disapperInSeconds);
+            if (text)
+                $("#overlay span").text(text);
+            if (disapperInSeconds)
+                setTimeout(utils.overlay.off, 1000 * disapperInSeconds);
         },
-        off:function(){
+        off: function() {
             $("#overlay span").text("Please wait ...");
             document.getElementById("overlay").style.display = "none";
         }
     },
-    getLongUrlFormat:function(input){
+    getLongUrlFormat: function(input) {
         var maxLength = 20;
         input = input || "";
-        if(input.length > maxLength){
-            return input.substr(0,maxLength-1)+"...";
+        if (input.length > maxLength) {
+            return input.substr(0, maxLength - 1) + "...";
         }
         return input;
     },
-    copyToClipboard:function (text) {
+    copyToClipboard: function(text) {
         var input = document.body.appendChild(document.createElement("input"));
         input.value = text;
         input.focus();
@@ -38,81 +39,87 @@ var utils = {
 }
 // end of utils
 
-function initlizeApp(){
-    
+function initlizeApp() {
+
     //url list
-    var urlList  =ko.observableArray([])
-    var vm = {urlList:urlList, formatLongUrl:function(input){return utils.getLongUrlFormat(input)}};
-    vm.copySUrl=function(data){
+    var urlList = ko.observableArray([])
+    var vm = {
+        urlList: urlList,
+        formatLongUrl: function(input) {
+            return utils.getLongUrlFormat(input)
+        }
+    };
+    vm.copySUrl = function(data) {
         utils.copyToClipboard(data.s_url);
-        utils.overlay.on("copied successfully",1);
+        utils.overlay.on("copied successfully", 1);
     }
     ko.applyBindings(vm);
-    
-    
+
+
     // set/get local storage
     var userId = window.localStorage.getItem('user-id');
-    if(!window.localStorage) alert('local storage not availeble')
-    if(!userId) localStorage.setItem('user-id',utils.getUuidv4())
-    
+    if (!window.localStorage) alert('local storage not availeble')
+    if (!userId) {
+        userId = utils.getUuidv4();
+        localStorage.setItem('user-id', userId);
+    }
+
     //event listeners
-    
-    window.submitForm=function () {
+
+    window.submitForm = function() {
         utils.overlay.on();
         var l_url = $("#txtLongUrl").val();
-        if(l_url){
+        if (l_url) {
             var data = {
-                l_url : l_url,
-                usr :userId
+                l_url: l_url,
+                usr: userId
             };
             $.ajax({
-                url:"/url",
-                dataType : "json",
-                method : "POST",
+                url: "/url",
+                dataType: "json",
+                method: "POST",
                 contentType: "application/json; charset=utf-8",
-                data : JSON.stringify(data),
-                success : function(result) {
+                data: JSON.stringify(data),
+                success: function(result) {
                     utils.overlay.on('Created successfully');
                     $("#txtLongUrl").val('')
                     loadData()
                 },
-                complete:function(){
+                complete: function() {
                     utils.overlay.off();
-                }            
+                }
             })
         }
     }
-    function loadData(){
+
+    function loadData() {
         utils.overlay.on('Loading data...');
-        
+
         $.ajax({
-            url:"/url?user-id="+userId,
-            dataType : "json",
-            method : "GET",
+            url: "/url?user-id=" + userId,
+            dataType: "json",
+            method: "GET",
             contentType: "application/json; charset=utf-8",
-            success : function(result) {
-                if(result && Array.isArray(result)){
+            success: function(result) {
+                if (result && Array.isArray(result)) {
                     urlList(result);
                 }
             },
-            complete:function(){
+            complete: function() {
                 utils.overlay.off();
             }
         })
     }
-    
-    
+
+
     utils.overlay.off();
     loadData();
-    
-    
+
+
 }
 
 
 
-$(document).ready(function(){
+$(document).ready(function() {
     initlizeApp();
-})
-
-
-
+});
--- routes/service.js
+++ routes/service.js
@@ -1,5 +1,7 @@
 const url = require('url');
-const { Pool } = require('pg');
+const {
+    Pool
+} = require('pg');
 
 const pool = new Pool({
     user: process.env.DB_user,
@@ -10,64 +12,83 @@ const pool = new Pool({
 })
 
 function makeid(length) {
-    var result           = '';
-    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var result = '';
+    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
     var charactersLength = characters.length;
-    for ( var i = 0; i < length; i++ ) {
+    for (var i = 0; i < length; i++) {
         result += characters.charAt(Math.floor(Math.random() * charactersLength));
     }
     return result;
 }
 
-exports.getMyShortUrls = function (userId, callBack) {
-    pool.connect((err, client, done) => {
-        if (err) {callBack(err, null);}
-        else{
-            
-            const query="select * from urls where usr=$1";
-            const values = [userId];
-            client.query(query,values, (err, res) => {
-                done()
-                if (err) callBack(err, null);
-                else callBack(null, res.rows);
-            })
-        }
-        
-    })
-};
-exports.getMyShortUrl = function (s_url, callBack) {
-    pool.connect((err, client, done) => {
-        if (err) {callBack(err, null);}
-        else{
-            
-            const query="select l_url from urls where s_url=$1";
-            const values = [s_url];
-            client.query(query,values, (err, res) => {
-                if (err) {callBack(err, null);}
-                else {callBack(null, res.rows.length === 0 ? null : res.rows[0]["l_url"]);
-                const updCntQr= "update urls set c_cnt=c_cnt+1 where s_url=$1";
-                client.query(updCntQr,values, (errI, resI) => {
+function isValidUuid4(input) {
+    return /^[0-9A-F]{8}-[0-9A-F]{4}-[4][0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i.test(input);
+}
+
+exports.getMyShortUrls = function(userId, callBack) {
+    if (!userId || !isValidUuid4(userId)) callBack(null, []);
+    else {
+        pool.connect((err, client, done) => {
+            if (err) {
+                callBack(err, null);
+            } else {
+
+                const query = "select * from urls where usr=$1";
+                const values = [userId];
+                client.query(query, values, (err, res) => {
                     done()
+                    if (err) callBack(err, null);
+                    else callBack(null, res.rows);
                 })
             }
+
         })
-    }
-    
-})
+    };
+};
+exports.getMyShortUrl = function(s_url, callBack) {
+    pool.connect((err, client, done) => {
+        if (err) {
+            callBack(err, null);
+        } else {
+
+            const query = "select l_url from urls where s_url=$1";
+            const values = [s_url];
+            client.query(query, values, (err, res) => {
+                if (err) {
+                    callBack(err, null);
+                } else {
+                    callBack(null, res.rows.length === 0 ? null : res.rows[0]["l_url"]);
+                    const updCntQr = "update urls set c_cnt=c_cnt+1 where s_url=$1";
+                    client.query(updCntQr, values, (errI, resI) => {
+                        done()
+                    })
+                }
+            })
+        }
+
+    })
 };
 
-exports.makeShortUrl = function (baseUrl, body, callBack) {
-    if(!body.l_url) callBack({msg:'invalid l_url'}, null);
-    if(!body.usr) callBack({msg:'invalid usr'}, null);
-    pool.connect((err, client, done) => {
-        if (err) callBack(err, null);
-        let s_url = baseUrl + "/" + makeid(7);
-        const query="insert into urls(l_url,s_url,c_cnt,usr) values($1,$2,$3,$4)";
-        const values=[body.l_url,s_url,0,body.usr];
-        client.query(query,values, (err, res) => {
-            done();
+exports.makeShortUrl = function(baseUrl, body, callBack) {
+    if (!body.l_url || !isValidUuid4(body.l_url)) callBack({
+        msg: 'invalid l_url'
+    }, null);
+    else if (!body.usr) callBack({
+        msg: 'invalid usr'
+    }, null);
+    else {
+        pool.connect((err, client, done) => {
             if (err) callBack(err, null);
-            else callBack(null, {s_url : s_url});
+            let s_url = baseUrl + "/" + makeid(7);
+            const query = "insert into urls(l_url,s_url,c_cnt,usr) values($1,$2,$3,$4)";
+            const values = [body.l_url, s_url, 0, body.usr];
+            client.query(query, values, (err, res) => {
+                done();
+                if (err) callBack(err, null);
+                else callBack(null, {
+                    s_url: s_url
+                });
+            })
         })
-    })
+    }
 };
